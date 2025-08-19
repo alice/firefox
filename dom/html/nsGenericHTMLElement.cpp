ac8831fd325679457910bf496d4fb3be36153a63
Alice Boxhall <95208+alice@users.noreply.github.com>
2025-08-19 12:51:53 +0200

    Bug 1981344 - Implement referenceTarget support for single-element attributes. r?#dom-core
    Add referenceTarget support for single element attributes: list, popoverTarget and commandFor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D261365

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -2823,13 +2823,13 @@ bool nsGenericHTMLFormControlElementWithState::ParseAttribute(
 mozilla::dom::Element*
 nsGenericHTMLFormControlElementWithState::GetPopoverTargetElementForBindings()
     const {
-  return GetPopoverTargetElementInternal();
+  return GetAttrAssociatedElementForBindings(nsGkAtoms::popovertarget);
 }
 
 mozilla::dom::Element*
 nsGenericHTMLFormControlElementWithState::GetPopoverTargetElementInternal()
     const {
-  return GetAttrAssociatedElement(nsGkAtoms::popovertarget);
+  return GetAttrAssociatedElementInternal(nsGkAtoms::popovertarget);
 }
 
 void nsGenericHTMLFormControlElementWithState::
